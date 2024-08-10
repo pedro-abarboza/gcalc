8be866bec361f1440bf90a76153f4088327107d6
pedro <pedro.abarboza@gmail.com>
2024-08-10 08:48:17 -0300

    Teste dos statics

--- core/settings.py
+++ core/settings.py
@@ -120,8 +120,8 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR / 'static/'),)
+STATIC_URL = '/statics/'
+STATICFILES_DIRS = (str(BASE_DIR / 'statics/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
