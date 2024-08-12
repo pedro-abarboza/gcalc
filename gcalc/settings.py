71e65a808ed973dbf9c7cfd617f65e726ea9c4f1
pedro <pedro.abarboza@gmail.com>
2024-08-12 16:27:39 -0300

    Reinicio

--- gcalc/settings.py
+++ gcalc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nxq*jen(^qmggk+ou2qwx7yyscpnvbb7fzy2!$aww*r5d_phh+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
