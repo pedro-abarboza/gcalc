12a95f6001f40cad770ef992fce05dfb2086dd3d
pedro <pedro.abarboza@gmail.com>
2024-08-12 16:25:09 -0300

    Reinicio

--- gcalc/settings.py
+++ gcalc/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = 'gcalc.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = 'gcalc.wsgi.application'
 
 
 # Database
